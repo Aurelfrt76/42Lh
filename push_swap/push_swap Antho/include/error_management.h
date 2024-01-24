3d0bc3554b84c6a0325b6ecd8da54dbcede9b5db
Aurelien Fromont <afromont@c1r6p15.42lehavre.fr>
2024-01-24 15:01:57 +0100

    deleted pushswap

--- push_swap/push_swap Antho/include/error_management.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERROR_MANAGEMENT_H
-# define ERROR_MANAGEMENT_H
-
-void	exit_with_error(const char *err_msg);
-
-#endif
--- push_swap/push_swap Antho/src/error_management.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "error_management.h"
-#include "../LIB/ft_printf.h"
-
-void	exit_with_error(const char *err_msg)
-{
-	ft_printf("%s", err_msg);
-	exit(1);
-}
